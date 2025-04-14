7ff53fea41f78f6a5fbd5a9044ef004d02487df9
tzzing <mmaribbay@ufl.edu>
2025-04-14 13:50:32 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -9,21 +9,20 @@ import math
 def add(a, b):
     return a+b
 
-def multiply(a, b):
-    return a*b
+def subtract(a, b):
+    return a-b
+
+
 
 def logarithim(a,b):
     if b<0:
         raise ValueError
     return math.log(b,a)
-def exponent(a,b):
-    return a**b
+
 import math
 def add(a, b):
     return a+b
 
-def sub(a, b):
-    return a-b
 
 def mul(a, b):
     return a*b
@@ -32,11 +31,6 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b/a
-
-def log(a,b):
-   if b<1:
-       raise ValueError
-   return math.log(b, a)
 
 def exp(a,b):
    return a**b
